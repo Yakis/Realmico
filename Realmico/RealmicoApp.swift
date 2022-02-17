7bbdf57615439cbed6d3c86d94c1877516c33954
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2022-02-17 07:09:59 +0000

    Initial Commit

---
+++ Realmico/RealmicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmicoApp.swift
+//  Realmico
+//
+//  Created by Mugurel Moscaliuc on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RealmicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
